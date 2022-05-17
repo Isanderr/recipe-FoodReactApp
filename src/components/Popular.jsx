aff361d706bd9ac6a9e24abcec8fd2e6291b8353
Isanderr <isander@wp.pl>
2022-05-18 00:40:47 +0200

    details page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import styled from "styled-components";
 import {Splide, SplideSlide} from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
+import {Link} from 'react-router-dom';
 
 function Popular ()
 {
@@ -48,10 +49,12 @@ function Popular ()
           {
             return (
               <SplideSlide key={recipe.id}>
-              <Card>
+                <Card>
+                <Link to={"/recipe/" + recipe.id}>
                 <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
-                  <Gradient/>
+                    <Gradient />
+                    </Link>
                 </Card>
                 </SplideSlide>
             );
